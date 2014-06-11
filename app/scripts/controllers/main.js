2af86c3f8b12ddd91e787175107f0e64f514c4f3
Huntly Mayo <huntly.mm@gmail.com>
2014-06-10 20:21:28 -0400

    incorporate google auth and initial calendar data

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,6 +1,12 @@
 'use strict';
 
 angular.module('reminderApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $http, $location) {
     $scope.world = {};
+
+    $scope.google = '';
+    $http.get('googleauth').success(function(url) {
+        console.log(url.link);
+        $scope.google = url.link;
+      });
   });
